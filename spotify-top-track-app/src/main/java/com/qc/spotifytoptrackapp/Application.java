a8b6651a1882454e69fce7a05b452bcd39485b03
QC2028 <yaqoob@malik5.com>
2025-05-04 14:56:33 +0100

    initialise Maven project

---
+++ spotify-top-track-app/src/main/java/com/qc/spotifytoptrackapp/Application.java
@@ -0,0 +1,10 @@
+package com.qc.spotifytoptrackapp;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
